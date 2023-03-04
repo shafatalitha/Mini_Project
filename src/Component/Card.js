84f9ec0c55287de259b3c6d1664393b3fa6d62ec
unknown <shafathalitha@gmail.com>
2023-03-04 14:51:20 +0700

    buat card

---
+++ src/Component/Card.js
@@ -0,0 +1,11 @@
+import "./styles.css"
+const Card =()=>{
+    return (
+        <div className="kotak">
+            <h1>Hi, saya Shafa</h1>
+            <p>saya dari batch 36 berhasil menginstall react</p>
+        </div>
+    )
+}
+
+export default Card;
