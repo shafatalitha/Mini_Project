1f2a80f398992f920cadb4beff07a7b54fbf3ecf
unknown <shafathalitha@gmail.com>
2023-03-05 09:21:17 +0700

    Uset tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import Card from './Component/Card';
-import Card2 from './Component/Card2';
-import Counter from './Component/Counter';
+import Card from "./Component/Card";
+import Card2 from "./Component/Card2";
+import Counter from "./Component/Counter";
+import Table from "./Component/Table";
+
 function App() {
   return (
     <div className="App">
-      <Card/>
-      <Card2/>
-      <Counter/>
+      <Card />
+      <Card2 />
+      <Counter />
+      <Table/>
+     
     </div>
   );
 }
